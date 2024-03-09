cf138cdd175b0668ec6d7bdec25b72880da02e2e
junyu33 <helloworld@junyu33.me>
2024-03-09 03:41:44 +0000

    update start.sh

--- README.md
+++ README.md
@@ -11,8 +11,10 @@ This is a simple implementation of the GMW protocol in python. The GMW protocol 
 modify your gates in the `gates.py` file, and then run `start.sh`. The result will be printed to the console.
 
 ```sh
-./start.sh <value of x> <value of y> [<ip of bob>] [<port>]
+./start.sh <person> <bit> [<ip of bob>] [<port>]
 ```
+
+where 0 stands for Alice and 1 stands for Bob in `person` arg. 
 
 ## references
 
--- start.sh
+++ start.sh
@@ -1,27 +1,34 @@
 #!/bin/sh
-# set x to 1st argument, y to 2nd argument, and port to 3rd argument
-x=$1
-y=$2
+# 0 for alice, 1 for bob 
+person=$1
+bit=$2
 ip=$3
 port=$4
 
 # if x or y is not 0 or 1, then exit
-if [ $x -ne 0 ] && [ $x -ne 1 ]; then
-    echo "x must be 0 or 1"
-    exit 1
-fi
-
-if [ $y -ne 0 ] && [ $y -ne 1 ]; then
-    echo "y must be 0 or 1"
+if [ $bit -ne 0 ] && [ $bit -ne 1 ]; then
+    echo "bit must be 0 or 1"
     exit 1
 fi
 
 # if port is a number, then call python with x, y, and port
 if [ $port -eq $port 2>/dev/null ]; then
-    ./bob.py $y $ip $port & 
-    ./alice.py $x $ip $port 
+    if [ $person -eq 1 ]; then
+        ./bob.py $bit $ip $port
+    else
+        ./alice.py $bit $ip $port 
+    fi
+elif [ $ip -eq $ip 2>/dev/null ]; then
+    if [ $person -eq 1 ]; then
+        ./bob.py $bit $ip 
+    else
+        ./alice.py $bit $ip 
+    fi
 else
-    ./bob.py $y &
-    ./alice.py $x 
+    if [ $person -eq 1 ]; then
+        ./bob.py $bit
+    else
+        ./alice.py $bit 
+    fi
 fi
 
